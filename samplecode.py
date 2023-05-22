70ad19de0e685019f82008bb8085c4aa2acc5fc5
Adhila Farsana <adhilafarsana@gmail.com>
2023-05-22 14:03:35 +0530

    initial commit

---
+++ samplecode.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 22 13:46:44 2023
+
+@author: hp
+"""
+
+#include <stdio.h>
+int main() {   
+    int number;
+   
+    printf("Enter an integer: ");  
+    
+    // reads and stores input
+    scanf("%d", &number);
+
+    // displays output
+    printf("You entered: %d", number);
+    
+    return 0;
+}
